ed24760a7baf0fa0530735f0211113371bab82e8
Adriel23456 <adriel.chaves@hotmail.com>
2022-09-02 21:41:14 -0600

    Version Final 1.0

--- src/main/java/tarea_extraclase/tarea_extraclase/Main.java
+++ src/main/java/tarea_extraclase/tarea_extraclase/Main.java
@@ -4,12 +4,8 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
-
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
+
 
 public class Main extends Application {
     @Override
@@ -22,57 +18,7 @@ public class Main extends Application {
     }
 
     public static void main(String[] args) {
-        //--------------------------------------------------------------------
-        //--------------------------------------------------------------------
-        //Se establece el lugar desde el cual se va a leer el archivo CSV
-
-        String path = "Ejemplo.csv";
-
-        //--------------------------------------------------------------------
-        //--------------------------------------------------------------------
-
-        //Se crea un objeto tipo lista, para guradar la informacion del CSV en el. Se tiene que especificar el tipo de dato, en este caso sera un dato Estudiante
-        DoubleLinkedList<Estudiante> estudianteDoubleLinkedList = new DoubleLinkedList<>();
-
-        //Se crean objetos comodin para llamar a sus respectivos metodos recuperar() y se establecen con sus valores predeterminados
-        Estudiante_A comodin_A = new Estudiante_A();
-        Estudiante_B comodin_B = new Estudiante_B();
-
-        //Esta variable va a contener temporalmente los datos ingresados, o sea, el objeto actual
-        Estudiante estudiante = null;
-
-        //Se crea una linea vacia, para rellenar esta variable mas tarde con los elementos del csv por linea
-        String line = "";
-
-        //Se establece un objeto el cual va a intentar leer el archivo CSV, solamente 1 vez
-        try {
-            BufferedReader br = new BufferedReader(new FileReader(path));
-
-
-            //Método para leer todos los elementos del CSV y acomodarlos como objetos Estudiante
-            //ESTE MéTODO APLICA ENCAPSULAMIENTO, PUESTO QUE, VA A ENCAPSULAR UNA GRAN CANTIDAD DE DATOS EN UN SOLO NODO DE LA LISTA estudianteDoubleLinkedList
-            //Este método va a generar objetos desde la perspectiva de estudiantes, por lo cual estos objetos serán INSTANCIAS de esa clase
-            line = br.readLine();
-            while((line = br.readLine()) != null){
-                String [] values = line.split(",");
-                if (values[5].equals("A")){
-                    estudiante = comodin_A.recuperar(values);
-                }
-                else {
-                    estudiante = comodin_B.recuperar(values);
-                }
-                estudianteDoubleLinkedList.add(estudiante);
-            }
-            //Se imprime con el toString() los valores de los objetos creados, en orden
-            //System.out.println(estudianteDoubleLinkedList.toString());
-        }
-        //Para imprimir errores comunes que podria desarrollar el BufferedReader
-        catch (FileNotFoundException e){
-            e.printStackTrace();
-        }
-        catch (IOException e){
-            e.printStackTrace();
-        }
+        //Metodo el cual va a permitirle al metodo Plan_De_Contingencia() actuar y empezar el codigo
         launch();
     }
 }
--- src/main/java/tarea_extraclase/tarea_extraclase/VentanaController.java
+++ src/main/java/tarea_extraclase/tarea_extraclase/VentanaController.java
@@ -7,7 +7,6 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
-
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -17,61 +16,45 @@ import java.util.ResourceBundle;
 
 public class VentanaController implements Initializable{
     //--------------------------------------------------------------------
-    //--------------------------------------------------------------------
     //Se establece el lugar desde el cual se va a leer el archivo CSV
     String path = "Ejemplo.csv";
     //--------------------------------------------------------------------
     //--------------------------------------------------------------------
-
-    private DoubleLinkedList<Estudiante> estudianteDoubleLinkedList = new DoubleLinkedList<>();
+    //Se crea un objeto tipo lista, para guradar la informacion del CSV en el. Se tiene que especificar el tipo de dato, en este caso sera un dato Estudiante
+    DoubleLinkedList<Estudiante> estudianteDoubleLinkedList = new DoubleLinkedList<>();
+    //Se establecer los valores inciales del FXML
     @FXML
     private TableView<Estudiante> tableview;
     @FXML
     private TableColumn<Estudiante, Integer> idcarnet;
-
     @FXML
     private TableColumn<Estudiante, String> idcorreo;
-
     @FXML
     private TableColumn<Estudiante, String> idnickname;
-
     @FXML
     private TableColumn<Estudiante, String> idnombre;
-
     @FXML
     private TableColumn<Estudiante, Double> idnotaexamen;
-
     @FXML
     private TableColumn<Estudiante, Double> idnotafinal;
-
     @FXML
     private TableColumn<Estudiante, Double> idnotapromedioexamen;
-
     @FXML
     private TableColumn<Estudiante, Double> idnotapromedioproyecto;
-
     @FXML
     private TableColumn<Estudiante, Double> idnotaproyecto1;
-
     @FXML
     private TableColumn<Estudiante, Double> idnotaproyecto2;
-
     @FXML
     private TableColumn<Estudiante, Double> idnotaproyecto3;
-
     @FXML
     private TableColumn<Estudiante, Double> idnotaquiz;
-
     @FXML
     private TableColumn<Estudiante, Double> idnotatarea;
-
     @FXML
     private TableColumn<Estudiante, Integer> idtelefono;
-
     @FXML
     private TableColumn<Estudiante, String> idtipo;
-
-
     ObservableList<Estudiante> list = FXCollections.observableArrayList();
     public void llenar(){
                 Plan_De_Contingencia();
@@ -79,38 +62,54 @@ public class VentanaController implements Initializable{
                     list.add(estudianteDoubleLinkedList.getElement(i));
                 }
     }
+
     @Override
-    public void initialize(URL url, ResourceBundle resourceBundle)throws IllegalStateException{
+    public void initialize(URL url, ResourceBundle resourceBundle) throws IllegalStateException{
         llenar();
         try{
-            idcarnet.setCellValueFactory(new PropertyValueFactory<Estudiante,Integer>("carnet"));
-            idnombre.setCellValueFactory(new PropertyValueFactory<Estudiante,String>("nombre_Completo"));
-            idcorreo.setCellValueFactory(new PropertyValueFactory<Estudiante,String>("correo"));
-            idtelefono.setCellValueFactory(new PropertyValueFactory<Estudiante,Integer>("telefono"));
-            idnickname.setCellValueFactory(new PropertyValueFactory<Estudiante,String>("nickname"));
-            idtipo.setCellValueFactory(new PropertyValueFactory<Estudiante,String>("tipo_estudiante"));
-            idnotaexamen.setCellValueFactory(new PropertyValueFactory<Estudiante,Double>("nota_Promedio_Examenes"));
-            idnotaquiz.setCellValueFactory(new PropertyValueFactory<Estudiante,Double>("nota_Promedio_Quices"));
-            idnotatarea.setCellValueFactory(new PropertyValueFactory<Estudiante,Double>("nota_Promedio_Tareas"));
-            idnotaproyecto1.setCellValueFactory(new PropertyValueFactory<Estudiante,Double>("nota_Proyecto1"));
-            idnotaproyecto2.setCellValueFactory(new PropertyValueFactory<Estudiante,Double>("nota_Proyecto2"));
-            idnotaproyecto3.setCellValueFactory(new PropertyValueFactory<Estudiante,Double>("nota_Proyecto3"));
-            idnotapromedioproyecto.setCellValueFactory(new PropertyValueFactory<Estudiante,Double>("nota_B"));
-            idnotapromedioexamen.setCellValueFactory(new PropertyValueFactory<Estudiante,Double>("nota_A"));
-            idnotafinal.setCellValueFactory(new PropertyValueFactory<Estudiante,Double>("nota_Final"));
-            tableview.setItems(list);
+            for (Estudiante estudiante: list){
+                idcarnet.setCellValueFactory(new PropertyValueFactory<Estudiante,Integer>("carnet"));
+                idnombre.setCellValueFactory(new PropertyValueFactory<Estudiante,String>("nombre_Completo"));
+                idcorreo.setCellValueFactory(new PropertyValueFactory<Estudiante,String>("correo"));
+                idtelefono.setCellValueFactory(new PropertyValueFactory<Estudiante,Integer>("telefono"));
+                idnickname.setCellValueFactory(new PropertyValueFactory<Estudiante,String>("nickname"));
+                idtipo.setCellValueFactory(new PropertyValueFactory<Estudiante,String>("tipo_estudiante"));
+                if (estudiante instanceof Estudiante_A){
+                    System.out.println(estudiante instanceof Estudiante_A);
+                    idnotaexamen.setCellValueFactory(new PropertyValueFactory<Estudiante,Double>("nota_Promedio_Examenes"));
+                    idnotaquiz.setCellValueFactory(new PropertyValueFactory<Estudiante,Double>("nota_Promedio_Quices"));
+                    idnotatarea.setCellValueFactory(new PropertyValueFactory<Estudiante,Double>("nota_Promedio_Tareas"));
+                    idnotapromedioexamen.setCellValueFactory(new PropertyValueFactory<Estudiante,Double>("nota_A"));
+                } else {
+                    System.out.println(estudiante instanceof Estudiante_B);
+                    idnotaproyecto1.setCellValueFactory(new PropertyValueFactory<Estudiante,Double>("nota_Proyecto1"));
+                    idnotaproyecto2.setCellValueFactory(new PropertyValueFactory<Estudiante,Double>("nota_Proyecto2"));
+                    idnotaproyecto3.setCellValueFactory(new PropertyValueFactory<Estudiante,Double>("nota_Proyecto3"));
+                    idnotapromedioproyecto.setCellValueFactory(new PropertyValueFactory<Estudiante,Double>("nota_B"));
+                }
+                idnotafinal.setCellValueFactory(new PropertyValueFactory<Estudiante,Double>("nota_Final"));
+                tableview.setItems(list);
+            }
         }
         catch (IllegalStateException x){
             System.out.println("PINGA MAMA");
         }
     }
-    public void Plan_De_Contingencia (){
+
+    public void Plan_De_Contingencia(){
+        //Se crean objetos comodin para llamar a sus respectivos metodos recuperar() y se establecen con sus valores predeterminados
         Estudiante_A comodin_A = new Estudiante_A();
         Estudiante_B comodin_B = new Estudiante_B();
+        //Esta variable va a contener temporalmente los datos ingresados, o sea, el objeto actual
         Estudiante estudiante = null;
+        //Se crea una linea vacia, para rellenar esta variable mas tarde con los elementos del csv por linea
         String line = "";
+        //Se establece un objeto el cual va a intentar leer el archivo CSV, solamente 1 vez
         try {
             BufferedReader br = new BufferedReader(new FileReader(path));
+            //Método para leer todos los elementos del CSV y acomodarlos como objetos Estudiante
+            //ESTE MéTODO APLICA ENCAPSULAMIENTO, PUESTO QUE, VA A ENCAPSULAR UNA GRAN CANTIDAD DE DATOS EN UN SOLO NODO DE LA LISTA estudianteDoubleLinkedList
+            //Este método va a generar objetos desde la perspectiva de estudiantes, por lo cual estos objetos serán INSTANCIAS de esa clase
             line = br.readLine();
             while((line = br.readLine()) != null){
                 String [] values = line.split(",");
@@ -123,6 +122,7 @@ public class VentanaController implements Initializable{
                 estudianteDoubleLinkedList.add(estudiante);
             }
         }
+        //Para imprimir errores comunes que podria desarrollar el BufferedReader
         catch (FileNotFoundException e){
             e.printStackTrace();
         }
