371ca7020b54d5cc7395f559658f79be3c102968
Adriel23456 <adriel.chaves@hotmail.com>
2022-09-02 21:43:28 -0600

    Version Final 1.1

--- src/main/java/tarea_extraclase/tarea_extraclase/Main.java
+++ src/main/java/tarea_extraclase/tarea_extraclase/Main.java
@@ -7,7 +7,9 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 
+//Aqui se desarrolla la clase main la cual va a permitir el funcionamiento de la aplicacion
 public class Main extends Application {
+    //Se añade el codigo base para abrir la escena del codigo
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Scene.fxml"));
@@ -16,7 +18,7 @@ public class Main extends Application {
         stage.setScene(scene);
         stage.show();
     }
-
+    //Se añade el metodo main el cual interactua con la clase VentanaController directamente
     public static void main(String[] args) {
         //Metodo el cual va a permitirle al metodo Plan_De_Contingencia() actuar y empezar el codigo
         launch();
